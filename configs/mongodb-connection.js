6975df788de5127037f1a74f93cd6ec16f9b8543
bottlesik <yh11445@gmail.com>
2023-12-14 13:07:37 +0900

    Update mongodb-connection.js

--- configs/mongodb-connection.js
+++ configs/mongodb-connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://yh11445:yvRKvS8v1w3xCG7U@cluster0.ac4lt1a.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<username>:<password>@cluster0.ac4lt1a.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = function (callback) {
     return MongoClient.connect(uri, callback);
